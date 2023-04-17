62d240a9a95ee08424c98bd1ff0fc85ac75ada39
Afshar Sharifi <afsharsharifi2020@gmail.com>
2023-04-17 14:53:00 +0330

    Converting to Divar Creawler

--- Django-Panel/DotWebPanel/DotWebPanelMain/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-from .views import index_page, user_signup, user_login, image_page
-
-urlpatterns = [
-    path('', index_page),
-    path('image/', image_page),
-    path('login/', user_login),
-    path('register/', user_signup),
-]
